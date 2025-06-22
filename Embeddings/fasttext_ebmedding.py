14e1ff21c7a403d0ae8e269914f15d22e57dc537
Abolfazl Aghdaee <abolfazlaghdaee2001@gmail.com>
2025-06-22 19:59:52 +0330

    change Embedding files

--- Embeddings/fasttext_ebmedding.py
+++ Embeddings/fasttext_ebmedding.py
@@ -1,23 +1,20 @@
 # !wget https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.fa.300.vec.gz
 # !gunzip cc.fa.300.vec.gz
-# !pip install numpy==1.23.5
-
-ft_model = KeyedVectors.load_word2vec_format('cc.fa.300.vec', binary=False)
 
 
 from gensim.models import KeyedVectors
 import numpy as np
+from tqdm import tqdm
 
-
-
+ft_model = KeyedVectors.load_word2vec_format('cc.fa.300.vec', binary=False)
 
 def get_sentence_vector_fasttext(sentence):
     words = sentence.split()
     vectors = [ft_model[word] for word in words if word in ft_model]
     return np.mean(vectors, axis=0) if vectors else np.zeros(ft_model.vector_size)
 
-
-
+def get_vectors_for_series(text_series):
+    return np.vstack([get_sentence_vector_fasttext(sentence) for sentence in tqdm(text_series, desc="Embedding sentences")])
 
 
 
--- Embeddings/parsbert_embedding.py
+++ Embeddings/parsbert_embedding.py
@@ -1,6 +1,7 @@
 from transformers import AutoTokenizer, AutoModel
 import torch
-
+import numpy as np
+from tqdm import tqdm
 
 model_name = "HooshvareLab/bert-base-parsbert-uncased"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
@@ -9,11 +10,26 @@ model = AutoModel.from_pretrained(model_name)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
-def get_parsbert_embeddings(texts):
-    tokenized_train = tokenizer(texts, padding=True, truncation=True, return_tensors="pt")
-    tokenized_train.to(device)
-    with torch.no_grad():
-        embeddings = model(**tokenized_train).last_hidden_state[0, 0, :].cpu().numpy()
-    return embeddings
+def get_parsbert_embeddings(texts, batch_size=32):
+    all_embeddings = []
 
+    for i in tqdm(range(0, len(texts), batch_size), desc="Embedding sentences"):
+        batch_texts = texts[i:i+batch_size]
 
+        # Tokenize batch
+        tokenized = tokenizer(
+            list(batch_texts),
+            padding=True,
+            truncation=True,
+            return_tensors="pt",
+            max_length=128 
+        ).to(device)
+
+        
+        with torch.no_grad():
+            outputs = model(**tokenized)
+            cls_embeddings = outputs.last_hidden_state[:, 0, :]
+
+        all_embeddings.append(cls_embeddings.cpu().numpy())
+
+    return np.vstack(all_embeddings)
